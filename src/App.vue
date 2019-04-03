84b4a3e0577ea89ffdc3d6e7a88f79efae099e49
Michael Byrd <michaelbyrdsemail@gmail.com>
2019-04-03 10:59:05 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="deck">
+      <div v-for="card in cards" :key="card.id">
+        <Card :value="card.rank" :suit="card.suit" />
+      </div>
+    </div>
+
+    <button @click="shuffle">shuffle</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card
+  },
+  data: function() {
+    return {
+      ranks: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+      suits: ['hearts', 'diamonds', 'spades', 'clubs'],
+      cards: []
+    }
+  },
+  created() {
+    this.initializeDeck();
+  },
+  methods: {
+    initializeDeck(){
+      let id=1;
+      for( let s = 0; s < this.suits.length; s++ ) {
+        for( let r = 0; r < this.ranks.length; r++ ) {
+          let card = {
+            id: id,
+            rank: this.ranks[r],
+            suit: this.suits[s]
+          }
+          this.cards.push(card);
+          id++;
+        }
+      }
+    },
+    shuffle(){
+      // Fisher-Yates shuffle
+      let currentIndex = this.cards.length, temporaryValue, randomIndex;
+      while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = this.cards[currentIndex];
+        // this.cards[currentIndex] = this.cards[randomIndex];
+        this.$set(this.cards, currentIndex, this.cards[randomIndex])
+        this.cards[randomIndex] = temporaryValue;
+      }
+
+      return this.cards;
+    }
+
   }
 }
 </script>
@@ -24,5 +71,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.deck {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card" :class="suitToColor()">
+    {{ value }}
+    {{ suitMap[suit] }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    value: String,
+    suit: String
+  },
+  data: function(){
+    return {
+      suitMap: {
+        hearts: '♥',
+        diamonds: '♦',
+        spades: '♠',
+        clubs: '♣',
+      }
+    }
+  },
+  methods: {
+    suitToColor(){
+      if(this.suit == 'hearts' || this.suit == 'diamonds'){
+        return 'red';
+      } else {
+        console.log('here');
+        return 'black';
+      }
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid black;
+    margin: .5em;
+    width: 3em;
+  }
+
+  .red {
+    color: darkred;
+    border-color: darkred;
+  }
+</style>
